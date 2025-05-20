4b521b5c79d072e686265f2823800a51ce9e5f0f
Tejas-987 <153412933+Tejas-987@users.noreply.github.com>
2025-05-20 12:19:41 +0530

    Time: 3 ms (2.85%), Space: 7.5 MB (100%) - LeetHub

---
+++ 1195-distribute-candies-to-people/1195-distribute-candies-to-people.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> distributeCandies(int candies, int num_people) {
+        vector<int> res(num_people);
+        for ( int index = 0; candies > 0; ++index, candies -= index )
+            res[index % num_people] += min(index + 1, candies);
+        return res;
+    }
+};
