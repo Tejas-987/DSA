bb14bc4ee0ae51aceb174cee0436fcf1a23eb11a
Tejas-987 <153412933+Tejas-987@users.noreply.github.com>
2025-05-21 13:46:46 +0530

    Time: 3 ms (55.39%), Space: 11 MB (69.97%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        // Step 1: Frequency map
         map<char, int> mp;
         for (int i = 0; i < s.size(); i++) {
             mp[s[i]]++;
         }
 
-        // Step 2: Max heap (priority queue) based on frequency
         priority_queue<pair<int, char>> pq;
         for (map<char, int>::iterator it = mp.begin(); it != mp.end(); it++) {
             pq.push(make_pair(it->second, it->first)); // frequency, character
         }
 
-        // Step 3: Build result using basic loops
         string result = "";
         while (!pq.empty()) {
             pair<int, char> p = pq.top();
